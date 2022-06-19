80c3e5bf450787b50c6b68388ab9109f8e343d80
hamid <hamidabdellaoui55@gmail.com>
2022-06-19 12:45:33 +0100

    update

--- Automating/airflow/dags/Main_dag.py
+++ Automating/airflow/dags/Main_dag.py
@@ -5,11 +5,44 @@ from airflow.models import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from includes.Scraping_news import scrap_lematin, scrap_challenge, scrap_lavieeco
+from includes.text_mining import *
+from includes.text_mining import data_frame_bigrams
 from airflow.utils.task_group import TaskGroup
 import pandas as pd
 import datetime
 from datetime import datetime, timedelta
 from airflow.operators.dummy_operator import DummyOperator
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+np.set_printoptions(precision=2, linewidth=80)
+from nltk import FreqDist
+
+# Gensim
+import gensim
+from gensim import corpora
+from gensim.models.coherencemodel import CoherenceModel
+
+import spacy
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk import ngrams
+import re
+#from bs4 import BeautifulSoup
+import unicodedata
+
+from spacy.lang.fr.stop_words import STOP_WORDS
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+import fr_core_news_md #import spacy french stemmer
+from sklearn.decomposition import NMF
+
+
+
 
 
 # Set arguments
@@ -31,11 +64,39 @@ def initial_processing():
     df = pd.concat([pd.read_csv('data/Raw/'+file) for file in files])
     df['date'] = pd.to_datetime(df['date'])
     df['date'] = df['date'].dt.strftime('%Y-%m-%d')
+    df['period'] = df['date'].to_period()
     df.to_csv('data/Dataset/data.csv',index=False, header=['titre','text','date','link','img'], sep=',')
     
-# def text_mining_preprocessing(**kwargs):
-#     df = pd.read_csv('data/Dataset/data.csv')
-#     df['text'] = df['text'].apply(lambda x: x.replace('\n', ' '))
+def corpuses_NMF ():
+    dataset = pd.read_csv('data/Dataset/data.csv')
+    print(type(dataset['date']))
+    print(dataset.info())
+    corpus = dataset.text.values.tolist()
+    corpuses_NMF = []
+    l_periods=list(set(dataset.period.values))
+    l_periods.sort()
+    for i in range(len(l_periods)-1) :
+        indice1=dataset.period.values.tolist().index(l_periods[i])
+        indice2=dataset.period.values.tolist().index(l_periods[i+1])
+        liste = []
+        for j in range(indice1,indice2,1):
+            liste.append(corpus[j]) 
+        corpuses_NMF.append(liste)
+
+    dernier_indice=dataset.period.values.tolist().index(l_periods[1])
+    derniere_corpus = [] 
+    for j in range(dernier_indice,len(dataset),1):
+        derniere_corpus.append(corpus[j]) 
+    corpuses_NMF.append(derniere_corpus)
+    
+    outputs =[]
+    for i in range(len(corpuses_NMF)): 
+        outputs.append(process_corpus(corpuses_NMF[i],i))
+    
+    print(type(outputs))
+    print(outputs)
+    # transform to json and push to next task (databases)
+    # return outputs
     
 dag = DAG(
     dag_id='Data_pipeline_for_text_mining',
@@ -52,26 +113,30 @@ with dag:
 
     with TaskGroup(group_id='scraping_data') as scraping_data:
         
-        scrap_challenge = PythonOperator(
-            task_id='scrap_challenge',
-            python_callable=scrap_challenge,
-            op_kwargs={'days': 60, 'nbr_pages':42},
-            # provide_context=True
-        )
+        # scrap_challenge = PythonOperator(
+        #     task_id='scrap_challenge',
+        #     python_callable=scrap_challenge,
+        #     op_kwargs={'days': 60, 'nbr_pages':42},
+        #     # provide_context=True
+        # )
         
-        scrap_lavieeco = PythonOperator(
-            task_id='scrap_lavieeco',
-            python_callable=scrap_lavieeco,
-            op_kwargs={'days': 60, 'nbr_pages':22}
-            # provide_context=True
-        )
+        # scrap_lavieeco = PythonOperator(
+        #     task_id='scrap_lavieeco',
+        #     python_callable=scrap_lavieeco,
+        #     op_kwargs={'days': 60, 'nbr_pages':22}
+        #     # provide_context=True
+        # )
           
-        scrap_lematin = PythonOperator(
-            task_id='scrap_lematin',
-            python_callable=scrap_lematin,
-            op_kwargs={'days': 60, 'nbr_pages':34}
-            # provide_context=True
-        )    
+        # scrap_lematin = PythonOperator(
+        #     task_id='scrap_lematin',
+        #     python_callable=scrap_lematin,
+        #     op_kwargs={'days': 60, 'nbr_pages':34}
+        #     # provide_context=True
+        # )    
+        t1 = DummyOperator(task_id='t1')
+        t2 = DummyOperator(task_id='t2')
+        t3 = DummyOperator(task_id='t3')
+  
     
     initial_processing = PythonOperator(
         task_id='initial_processing',
@@ -82,10 +147,17 @@ with dag:
     
     # Start Task Group definition
     with TaskGroup(group_id='text_mining') as text_mining:
-        t1 = DummyOperator(task_id='task1')
-        t2 = DummyOperator(task_id='task2')
-
-        t1 >> t2
+        corpuses_NMF = PythonOperator(
+            task_id='corpuses_NMF',
+            python_callable=corpuses_NMF,
+            # provide_context=True
+        )
+        data_frame_bigrams = PythonOperator(
+            task_id='data_frame_bigrams',
+            python_callable=data_frame_bigrams,
+            
+            )
+        [data_frame_bigrams , corpuses_NMF]
     # End Task Group definition
     
     end = DummyOperator(task_id='end')
--- Automating/airflow/dags/includes/text_mining.py
+++ Automating/airflow/dags/includes/text_mining.py
@@ -4,8 +4,6 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import re
 np.set_printoptions(precision=2, linewidth=80)
 from nltk import FreqDist
@@ -266,3 +264,92 @@ def getTopicTerms(pos_topics):
         topic_terms.append(terms) #ajoute l'ensemble des mots
     
     return topic_terms
+
+################ define process function ########################## 
+def preprocessing(corpus):
+    
+    corpus=remove_characters(corpus)
+    corpus=lower_text(corpus)
+    corpus=remove_stopwords(corpus)
+    corpus=lemm_tokens(corpus)
+    
+    return corpus
+
+def process_corpus(corpus,indice):
+    """retourne une liste output"""
+    ########### former la matrice tf-idf #############################
+    corpus_lemmatized=tokenize_text(corpus) 
+    id2word = corpora.Dictionary(corpus_lemmatized)
+    vocabulaire=id2word.token2id 
+    tfidf = TfidfVectorizer(use_idf=True, min_df=0.0, max_df=1.0,sublinear_tf=True,
+                        lowercase=True,ngram_range=(1,2),vocabulary=vocabulaire)
+    tfidf_train_features = tfidf.fit_transform(corpus)
+    feature_names = tfidf.get_feature_names()
+    ################# Topic Modelling : NMF ############################################
+    total_topics = 10
+    pos_nmf=NMF(n_components=total_topics,random_state=42,l1_ratio=0.2,max_iter=200)
+    pos_nmf.fit(tfidf_train_features) 
+    pos_weights = pos_nmf.components_
+    pos_topics = get_topics_terms_weights(pos_weights, feature_names)
+    topic_terms=getTopicTerms(pos_topics)
+    topic_terms=[topics[:20] for topics in topic_terms] 
+    common_corpus = [id2word.doc2bow(text) for text in corpus_lemmatized] 
+    ####### choix du meilleur model ###################################
+    cm = CoherenceModel(topics=topic_terms,corpus=common_corpus, dictionary=id2word, 
+               coherence='u_mass')
+    coherence = cm.get_coherence()
+    best_model,coherence_values=compute_coherence_values(tfidf_train_features,
+    feature_names,corpus,corpus_lemmatized,id2word,max_term=20,limit=50)
+    ######## résultat du modèle optimal ##############################
+    total_topics=best_model.n_components 
+    weights = best_model.components_ 
+    topics = get_topics_terms_weights(weights,feature_names)
+    ##### Data_For_Plot ########################################################
+    period = l_periods[indice]
+    dataset_modified = dataset.loc[dataset.period == period , :]
+    dates=pd.to_datetime(dataset_modified.loc[:,"date"].values)
+    doc_topic_dist = best_model.transform(tfidf_train_features) 
+    labels=getTopicTerms(topics)
+    labels=[",".join(topic_term[:5]) for topic_term in labels] 
+    df=pd.DataFrame({"text":corpus,"Date":dates,"doc_num":np.arange(len(corpus))})
+    stories=df.groupby("doc_num")["text","Date"].min().reset_index() 
+    story_topics_for_plot=pd.DataFrame(dict(doc_num=np.arange(doc_topic_dist.shape[0])))
+    for idx in range(len(labels)):
+        story_topics_for_plot[labels[idx]] = doc_topic_dist[:, idx]
+    trends = stories.merge(story_topics_for_plot, on='doc_num')
+    mass = lambda x: ((x) * 1.0).sum() / x.shape[0]  
+    window = 10
+    trend_indice = min(len(labels),5)
+    aggs = {labels[i]: mass for i in range(trend_indice) }
+    data_for_plot=trends.groupby(trends['Date'].dt.date).agg(aggs).rolling(window).mean()
+
+    ######### output de chaque corpus ###############################
+    output=[story_topics_for_plot,data_for_plot]
+
+    return output
+
+# define 
+
+def data_frame_bigrams():
+    dataset = pd.read_csv('data/Dataset/data.csv')
+    corpus = dataset.text.values.tolist()
+    """Create DataFrame with list of bigrams to be used in association rule learning with R"""
+    corpus = preprocessing(corpus)
+    list_bigrams = make_bigrams(corpus)
+    # Create a list of column names
+    columns_bigrams_df = []
+    for bigrams in list_bigrams :
+        for bigram in bigrams :
+            if bigram not in columns_bigrams_df :
+                columns_bigrams_df.append(bigram)
+    
+    # Create dataframe with bigrams
+    data_frame_bigrams = []
+    for bigrams in list_bigrams :
+        l_bigrams=[0 for i in range(len(columns_bigrams_df))]
+        for bigram in bigrams :
+            l_bigrams[columns_bigrams_df.index(bigram)]=1
+        data_frame_bigrams.append(l_bigrams)
+    
+    df = pd.DataFrame(data_frame_bigrams,columns=columns_bigrams_df)
+    df.to_csv("data/outputs/data_frame_bigrams.csv",index=False)
